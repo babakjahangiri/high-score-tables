328e082326dec80b4e5b52f56194bd905b0dbb9f
Babak <babakjahangiri@gmail.com>
2020-06-02 14:49:48 +0100

    Level 4 completed

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import PlayerScore from "./PlayerScore.js";
 import allCountryScores from "./data/scores";
 
 const HighScoreTable = (props) => {
+  const [order, setOrder] = useState("ASC");
+
+  function changeOrder() {
+    if (order === "ASC") {
+      setOrder("DESC");
+    }
+    if (order === "DESC") {
+      setOrder("ASC");
+    }
+  }
   return (
-    <div>
-      {allCountryScores
-        .sort((a, b) => (a.name > b.name ? 1 : -1))
-        .map((countryScore, index) => {
-          return (
-            <div key={index} className="high-score-table__container">
-              <PlayerScore
-                key={`${countryScore.name}+${index}`}
-                countryName={countryScore.name}
-                scores={countryScore.scores}
-              />
-            </div>
-          );
-        })}
-    </div>
+    <React.Fragment>
+      <button name="btn_order" onClick={changeOrder}>
+        {order === "ASC" ? "Ascending" : "Descending"}
+      </button>
+      <div>
+        {allCountryScores
+          .sort((a, b) => (a.name > b.name ? 1 : -1))
+          .map((countryScore, index) => {
+            return (
+              <div key={index} className="high-score-table__container">
+                <PlayerScore
+                  key={`${countryScore.name}+${index}`}
+                  countryName={countryScore.name}
+                  scores={countryScore.scores}
+                  orderType={order}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </React.Fragment>
   );
 };
 
--- src/PlayerScore.js
+++ src/PlayerScore.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 const PlayerScore = (props) => {
+  let sortedScores = props.scores;
+
+  if (props.orderType === "DESC") {
+    sortedScores = sortedScores.sort((a, b) => (a.s < b.s ? 1 : -1));
+  }
+  if (props.orderType === "ASC") {
+    sortedScores = sortedScores.sort((a, b) => (a.s > b.s ? 1 : -1));
+  }
+
   return (
     <div className="player-score__container">
       <p>HIGH SCORES: {props.countryName}</p>
-      {props.scores
-        .sort((a, b) => (a.s > b.s ? -1 : 1))
-        .map((scores, index) => {
-          return (
-            <div key={index} className="player-score_row">
-              <div>{scores.n.toUpperCase()} :</div>
-              <div>{scores.s}</div>
-            </div>
-          );
-        })}
+      {sortedScores.map((scores, index) => {
+        return (
+          <div key={index} className="player-score_row">
+            <div>{scores.n.toUpperCase()} :</div>
+            <div>{scores.s}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
